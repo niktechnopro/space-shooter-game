320fcdd435e94dcefebf4e6380791c2f70c25852
Nikolas Bogucarskis <niktechnopro@Nikolass-MacBook-Air.local>
2017-09-18 22:46:47 -0400

    multiple enemy ships update

--- enemies.py
+++ enemies.py
@@ -3,16 +3,19 @@ from pygame.sprite import Sprite
 from math import hypot
 import random
 
+
 class Enemies(Sprite):
-	def __init__(self,screen):
+	def __init__(self, screen, image):
 		super(Enemies,self).__init__()
-		self.image = pygame.image.load("images/enemyShip.png")
+		self.image = pygame.image.load(str(image))
+		self.image = pygame.transform.scale(self.image, (70, 70))
 		self.x = random.randrange(0,730,100)
 		self.y = 10
 		self.screen = screen
 		self.speed = 2
 		#self.rect = self.image.get_rect()
 		self.rect = pygame.Rect(self.x, self.y, 90, 70)
+		
 
 	def update_me(self, the_player):
 		self.y += self.speed
@@ -23,8 +26,10 @@ class Enemies(Sprite):
 	def draw_me(self):
 		self.screen.blit(self.image,[self.x,self.y])
 
-#	def explosion(self):
-#		self.image = pygame.image.load("images/explosion.png")
-#		self.x = x
-#		self.y = y
+	def explosion(self, bullets_x, bullets_y):
+		self.image = pygame.image.load("images/explosion.png")
+		bullets_x = self.x
+		bullets_y = self.y
+
+	
 
